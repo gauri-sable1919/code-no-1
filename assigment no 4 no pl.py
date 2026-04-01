2eac122f46d02fe7bd9c479fac989882b8423d9a
gauri-sable1919 <gourisabale2@gmail.com>
2026-04-01 21:46:04 +0530

    Add files via upload

---
+++ assigment no 4 no pl.py
@@ -0,0 +1,44 @@
+# Python program to add two numbers
+
+# Input from u 
+x=20
+y=30
+z=x+y
+print(z)
+
+
+a=10
+b=4.5
+c='Gauri'
+d= True
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+#Type of casting in Python
+
+k=5
+n=float(k)
+print(n)
+print(type(n))
+
+a=15.75
+b=int(a)
+print(b)
+print(type(b))
+
+x=123
+y=1.23
+z=x+y
+print(z)
+print(type(z))
+
+#Logical Operators
+
+g=True
+h=False
+print(g and h)
+print(g or h)
+# %%
+
---
+++ pl assigment no 1.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  1 19:06:31 2026
+
+@author: GAURI
+"""
+a = 10
+print(a,":",type(a))
+b = 2.5
+print(b,":",type(b))
+c = 'Hello World!'
+print(c,":",type(c))
+d = True
+print(d,":",type(d))
+e = []
+print(e,":",type(e))
+f = ()
+print(f,":",type(f))
+g = {}
+print(g,":",type(g))
+h = set()
+print(h,":",type(h))
+
+
